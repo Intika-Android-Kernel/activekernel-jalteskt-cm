007e5edd03c5edab4329ca68c9eca2258c6df4d5
bestmjh47 <bestmjh47@naver.com>
2014-12-30 19:20:13 +0900

    build: edited to build both cm and stock-based kernels, ramdisk: re-enable datafix

--- buildboot.sh
+++ buildboot.sh
@@ -1,8 +1,12 @@
 cd ramdisk/ramdisk-common
 find . | fakeroot cpio -H newc -o | lzma -e -9 > ../boot.img-ramdisk.cpio.lzma
 cd ../../
-mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/boot.img
+mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/data/media/cm.img
+cp -f $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/stock.img ramdisk/data/media/stock.img
+cp -f $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/stock.img ramdisk/data/media/boot.img
 cd ramdisk
-zip -r Multiboot-E300S-bestmjh47_kernel_CM.zip META-INF boot.img
-mv -v Multiboot-E300S-bestmjh47_kernel_CM.zip ../
+zip -r Multiboot_kernel_e300_v4.7.zip META-INF data boot.img
+mv -v Multiboot_kernel_e300_v4.7.zip ../
 cd ../
+rm -rf $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/cm.img
+cp -f ramdisk/data/media/cm.img $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/cm.img
