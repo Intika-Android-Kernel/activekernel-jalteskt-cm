94af4162de6782dd15255bd370073a7615cf0f81
bestmjh47 <bestmjh47@naver.com>
2015-02-23 14:51:04 +0900

    fix boot image packaging

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ find . | fakeroot cpio -H newc -o | lzma -e -9 > ../boot.img-ramdisk.cpio.lzma
 cd ../../
 mkbootimg --base 0x10000000 --pagesize 2048 --kernel zImage --ramdisk ramdisk/boot.img-ramdisk.cpio.lzma -o ramdisk/data/media/cm.img
 cp -f $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/stock.img ramdisk/data/media/stock.img
-cp -f $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/stock.img ramdisk/data/media/boot.img
+cp -f $HOME/kernel/activekernel-jalteskt/stock/ramdisk/data/media/stock.img ramdisk/boot.img
 cd ramdisk
 zip -r Multiboot_kernel_e300_v5.0.zip META-INF data boot.img
 mv -v Multiboot_kernel_e300_v5.0.zip ../
